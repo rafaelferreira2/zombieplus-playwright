71b062f714150e7e8e6cea351817a45cc4ca2a0d
Rafael Ferreira <sf.rafael20@gmail.com>
2025-03-18 21:09:30 -0300

    integração com Tesults

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,11 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [
+    ['list'],
+    ['playwright-tesults-reporter', {'tesults-target': process.env.TOKEN}]
+    // ['html']
+  ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -32,9 +36,10 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
-    screenshot: 'only-on-failure',
-    // video: 'on'
+    screenshot: 'on',
+    video: 'on',
     baseURL: process.env.BASE_URL,
+    viewport: { width: 1440, height: 900 }
   },
 
   /* Configure projects for major browsers */
